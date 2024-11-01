3aa7419cf829da2a7b481bf1a24a76d862093e8f
Siratul Islam <islamsiratul@gmail.com>
2024-11-01 23:44:04 +0600

    style: adjust formatting for consistency

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ export default async function Home() {
     <>
       <main className="p-1">
         <SignIn />
+        <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]" />
       </main>
     </>
   );
--- app/ui/Home.jsx
+++ app/ui/Home.jsx
@@ -65,55 +65,70 @@ export default function HomePage({ Routine }) {
     <div className="container mx-auto py-10 px-4">
       <h1 className="text-4xl font-bold mb-8 text-center">Available Classes</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {Routine.map((val) => (
+        {Routine.length > 0 ? (
           <>
-            <Card
-              key={val.id}
-              className="hover:shadow-lg transition-shadow duration-300"
-            >
-              <CardHeader>
-                <CardTitle className="flex justify-between items-center">
-                  <span> Batch : {val.Batch}</span>
-                  <Badge variant="outline">{val.Status}</Badge>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="space-y-2">
-                  <div className="flex items-center">
-                    <DoorOpen className="mr-2 h-4 w-4 text-muted-foreground" />
-                    <span> Room : {val.RoomNumber}</span>
-                  </div>
-                  <div className="flex items-center">
-                    <CalendarFold className="mr-2 h-4 w-4 text-muted-foreground" />
-                    <span> Day : {val.Day}</span>
-                  </div>
-                  <div className="flex items-center">
-                    <Users className="mr-2 h-4 w-4 text-muted-foreground" />
-                    <span>Section : {val.Section}</span>
-                  </div>
-                  <div className="flex items-center">
-                    <Clock className="mr-2 h-4 w-4 text-muted-foreground" />
-                    <span>
-                      {val.StartedTime} - {val.EndTime}
-                    </span>
-                  </div>
-                </div>
-              </CardContent>
-              <CardFooter>
-                <form className="w-full" action={updatePostAction}>
-                  <input id="id" name="id" value={val._id} className="hidden" />
-                  <input
-                    id="id"
-                    name="Status"
-                    value="Busy"
-                    className="hidden"
-                  />
-                  <Submit />
-                </form>
-              </CardFooter>
-            </Card>
+            {Routine.map((val) => (
+              <>
+                <Card
+                  key={val.id}
+                  className="hover:shadow-lg transition-shadow duration-300"
+                >
+                  <CardHeader>
+                    <CardTitle className="flex justify-between items-center">
+                      <span> Batch : {val.Batch}</span>
+                      <Badge variant="outline">{val.Status}</Badge>
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="space-y-2">
+                      <div className="flex items-center">
+                        <DoorOpen className="mr-2 h-4 w-4 text-muted-foreground" />
+                        <span> Room : {val.RoomNumber}</span>
+                      </div>
+                      <div className="flex items-center">
+                        <CalendarFold className="mr-2 h-4 w-4 text-muted-foreground" />
+                        <span> Day : {val.Day}</span>
+                      </div>
+                      <div className="flex items-center">
+                        <Users className="mr-2 h-4 w-4 text-muted-foreground" />
+                        <span>Section : {val.Section}</span>
+                      </div>
+                      <div className="flex items-center">
+                        <Clock className="mr-2 h-4 w-4 text-muted-foreground" />
+                        <span>
+                          {val.StartedTime} - {val.EndTime}
+                        </span>
+                      </div>
+                    </div>
+                  </CardContent>
+                  <CardFooter>
+                    <form className="w-full" action={updatePostAction}>
+                      <input
+                        id="id"
+                        name="id"
+                        value={val._id}
+                        className="hidden"
+                      />
+                      <input
+                        id="id"
+                        name="Status"
+                        value="Busy"
+                        className="hidden"
+                      />
+                      <Submit />
+                    </form>
+                  </CardFooter>
+                </Card>
+              </>
+            ))}
           </>
-        ))}
+        ) : (
+          <>
+            <div className="h-[34vh]">
+              <b className="text-lg">No data available</b>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
